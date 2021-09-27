715f0b67aaf78746f5eae542267c2b92d99fd140
bakkenal <>
2021-09-27 17:26:22 -0500

    added color

--- src/App.js
+++ src/App.js
@@ -33,15 +33,21 @@ const Banner = ({ title }) => (
 
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   
   return (
     <>
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      { 
+        termCourses.map(course =>
+          <Course key={ course.id } course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />) 
+      }
       </div>
-   </>
+    </>
   );
 };
 
@@ -75,14 +81,27 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
+
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const style = {
+    backgroundColor: isSelected ? 'lightgreen' : 'white'
+  };
+  return (
+    <div className="card m-1 p-2" 
+      style={style}
+      onClick={() => setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
